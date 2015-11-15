f408b923962d19b64c4c0a19be9df544c68cca75
Christian Encarnacion <encarc@rpi.edu>
2015-11-15 10:10:50 -0500

    Modified createLevel.py to play requested song on launch

--- createLevel.py
+++ createLevel.py
@@ -4,6 +4,7 @@ import time
 import math
 import os
 
+
 pygame.init()
 BLACK = (0,0,0)
 RED = (255,255,255)
@@ -14,8 +15,8 @@ startTime = 0
 currentTime = 0
 started = True
 windowSurface.fill(BLACK)
-gameLoop = True
 fName = 'notes.txt'
+pygame.mixer.music.load('./public/assets/pokemon_theme_song.ogg')
 
 f = open(fName, 'w')
 
@@ -25,57 +26,65 @@ def calculateMS():
 def writeToFile(sumNum, timeTaken):
 	f.write("[" + str(sumNum) + "," + str(timeTaken) + "]," + "\n")
 
-f.write("var arrayLevel = [")
-while gameLoop:
-	if(started):
-		startTime = time.time()
-		started = False
-	events = pygame.event.get()
-	for event in events:
-		if(event.type == pygame.KEYDOWN):
-			if(event.key == pygame.K_h):
-				writeToFile(1, calculateMS())
-			elif(event.key == pygame.K_j):
-				writeToFile(2, calculateMS())
-			elif(event.key == pygame.K_k):
-				writeToFile(4, calculateMS())
-			elif(event.key == pygame.K_l):
-				writeToFile(8, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_j):
-				writeToFile(3, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_k):
-				writeToFile(5, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_l):
-				writeToFile(9, calculateMS())
-			elif(event.key == pygame.K_j and event.key == pygame.K_k):
-				writeToFile(6, calculateMS())
-			elif(event.key == pygame.K_j and event.key == pygame.K_l):
-				writeToFile(10, calculateMS())
-			elif(event.key == pygame.K_k and event.key == pygame.K_l):
-				writeToFile(12, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_k):
-				writeToFile(7, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_l):
-				writeToFile(11, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_k and event.key == pygame.K_l):
-				writeToFile(13, calculateMS())
-			elif(event.key == pygame.K_j and event.key == pygame.K_k and event.key == pygame.K_l):
-				writeToFile(14, calculateMS())
-			elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_k and event.key == pygame.K_l):
-				writeToFile(15, calculateMS())
-			elif(event.key == pygame.K_c):
-				gameLoop = False
 
-f.write("];\nmodule.exports.arrayLevel = arrayLevel;")
-f.close()
+def main():
+    gameLoop = True
+    started = True
+    f.write("var arrayLevel = [")
+    while gameLoop:
+        if(started):
+            pygame.mixer.music.play(-1,0)
+            startTime = time.time()
+            started = False
+
+        events = pygame.event.get()
+        for event in events:
+            if(event.type == pygame.KEYDOWN):
+                if(event.key == pygame.K_h):
+                    writeToFile(1, calculateMS())
+                elif(event.key == pygame.K_j):
+                    writeToFile(2, calculateMS())
+                elif(event.key == pygame.K_k):
+                    writeToFile(4, calculateMS())
+                elif(event.key == pygame.K_l):
+                    writeToFile(8, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_j):
+                    writeToFile(3, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_k):
+                    writeToFile(5, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_l):
+                    writeToFile(9, calculateMS())
+                elif(event.key == pygame.K_j and event.key == pygame.K_k):
+                    writeToFile(6, calculateMS())
+                elif(event.key == pygame.K_j and event.key == pygame.K_l):
+                    writeToFile(10, calculateMS())
+                elif(event.key == pygame.K_k and event.key == pygame.K_l):
+                    writeToFile(12, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_k):
+                    writeToFile(7, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_l):
+                    writeToFile(11, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_k and event.key == pygame.K_l):
+                    writeToFile(13, calculateMS())
+                elif(event.key == pygame.K_j and event.key == pygame.K_k and event.key == pygame.K_l):
+                    writeToFile(14, calculateMS())
+                elif(event.key == pygame.K_h and event.key == pygame.K_j and event.key == pygame.K_k and event.key == pygame.K_l):
+                    writeToFile(15, calculateMS())
+                elif(event.key == pygame.K_c):
+                    gameLoop = False
+
+    f.write("];\nmodule.exports.arrayLevel = arrayLevel;")
+    f.close()
 
 
-files = os.listdir(os.curdir)
-for file in files:
-    if '.txt' in file:
-        if(os.path.isfile(fName[:-4] + '.js')):
-            os.remove(os.curdir + '/notes.js')
-        newfile = file.replace('.txt', '.js')
-        os.rename(file, newfile)
-        break
-        
+    files = os.listdir(os.curdir)
+    for file in files:
+        if '.txt' in file:
+            if(os.path.isfile(fName[:-4] + '.js')):
+                os.remove(os.curdir + '/notes.js')
+            newfile = file.replace('.txt', '.js')
+            os.rename(file, newfile)
+            break
+                
+main()
+
