2fc8106f7e41393df6aad9b0c0ba213654c9a448
Christian Encarnacion <encarc@rpi.edu>
2015-11-14 21:55:39 -0500

    messed around with User model

--- Models/User.js
+++ Models/User.js
@@ -1,26 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var progressSchema = new Schema(
-    {
-        songName: String,
-        easyScore: Number,
-        mediumScore: Number, 
-        hardScore: Number
-    }
-);
-
 var userSchema = new Schema(
     {
         username: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         
         
-        progress: [progressSchema],
+        progress: [],
         
         created_at: Date,
-        updated_at: Date,
-
+        updated_at: Date
     }
 );
 
@@ -51,29 +41,22 @@ userSchema.pre('save', function(next) {
     this.updated_at = currentDate;
 
     if(!this.created_at) this.created_at = currentDate;
-
+    
     next();
 });
 
-userSchema.methods.updateProgress = function(progressItem)
-{
-    this.progress.push(progressItem);
+/*userSchema.Progress = {
+    songName: "",
+    easyScore: "",
+    mediumScore: "",
+    hardScore: ""
 };
 
-userSchema.methods.defineProgress = function(sName, eScore, mScore, hScore)
-{
-    var progressObj = new progressSchema(
-    {
-        songName: sName,
-        easyScore: eScore,
-        mediumScore: mScore,
-        hardScore: hScore
-    })
+userSchema.methods.defineProgress = function(sName, eScore, mScore, hScore) {
     
-    
-    this.updateProgress(progressObj);
-};
-
+    var p = 
+}*/
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
--- test.js
+++ test.js
@@ -1,22 +1,37 @@
 var User = require('./Models/User');
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1/vim_ddr', function(err){
-    if(err) console.trace("FAILED TO CONNECT"+err);
+mongoose.connect('mongodb://localhost/vim_ddr', function(err){
+    if(err) {console.trace("FAILED TO CONNECT"+err);}
+    console.log('Connected to database...');
+
 });
 
+var db = mongoose.connection;
 var Jason = new User({
     username: "LimitedWard",
     password: "Lady'sman1357"
-});
-
-
-Jason.save(function(err) {
-    if (err) throw err;
     
-    console.log("User created");
 });
 
-//console.log(Jason.getName());
 
-mongoose.disconnect();
+
+db.once('open', function() {
+    Jason.save(function(err) {
+        if (err) throw err;
+        else {
+            //Jason.defineProgress("Hello", 2, 3, 4);
+
+            //Jason.progress.push(Progress);
+            console.log(Jason.getName());
+            console.log(Jason.getUpdatedDate());
+            console.log(Jason.progress);
+            console.log("User created");
+
+
+
+            //mongoose.disconnect();
+        }
+});
+});
+
