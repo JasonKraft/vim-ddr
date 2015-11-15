0c111c0c0a479f041759cfc61e38695d9f4cc8b9
Jason Kraft <kraftj3@rpi.edu>
2015-11-15 04:29:14 -0500

    Login stuff (work in progress)

--- vim_ddr/app.js
+++ vim_ddr/app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var passport = require('passport');
+var passportLocal = require('passport-local');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,14 +27,27 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(expressSession({
+	secret : process.env.SESSION_SECRET || 'dickbutt',
+	resave : false,
+	saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new passportLocal.Strategy(function(username, password, done) {
+	
+}));
+
+// app.use('/', routes);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -38,23 +55,39 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+app.get('/', function(req, res, next) {
+	res.render('index', {
+		title: 'Express',
+		isAuthenticated : false,
+		user : req.user
+	});
+});
+
+app.get('/login', function(req, res, next) {
+	res.render('login', { title: 'Login' });
+});
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+
 });
 
 
--- vim_ddr/routes/index.js
+++ vim_ddr/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	title: 'Express',
+  	isAuthenticated : false,
+  	user : req.user
+  });
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: 'Login' });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+
 });
 
 module.exports = router;
